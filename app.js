d4819dc30e2ea7c047b10da92373b4efe355bc58
Andres Gutierrez <andres@redbooth.com>
2015-09-05 01:23:56 +0200

    Initial express app with eslint super tuned

---
+++ app.js
@@ -0,0 +1,65 @@
+var app
+  , bodyParser
+  , cookieParser
+  , express
+  , logger
+  , path
+  , routes
+  , users;
+
+express = require('express');
+app = express();
+path = require('path');
+logger = require('morgan');
+cookieParser = require('cookie-parser');
+bodyParser = require('body-parser');
+routes = require('./routes/index');
+users = require('./routes/users');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err;
+
+  err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message
+    , error: err
+    });
+  });
+}
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message
+  , error: {}
+  });
+});
+
+module.exports = app;
